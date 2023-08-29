468998dedc9197e0932e116b4a0db81a6bbbf3f7
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-28 23:58:40 -0700

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'classes/retrieve_text_input.dart';
-import 'design/order_food.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: ExampleDragAndDrop(),
+        body: Container(), // replace it
       ),
       debugShowCheckedModeBanner: false,
     );
